b832a3f89b2ba156fdc7b5fb2a4b70602514ab42
Rate980 <2220208@ecc.ac.jp>
2024-10-11 10:33:23 +0900

    fix データサイズをちゃんと取るようにした

--- recorder/recorder/writer.py
+++ recorder/recorder/writer.py
@@ -18,6 +18,9 @@ class SerialWriter(Writer):
 
     def write(self, data: bytes) -> None:
         time.sleep(0.001)
-        num = self.serial.write(sliplib.encode(data))
+        data = sliplib.encode(data)
+        num = self.serial.write(data)
         if num != len(data):
             raise Exception("Failed to write data")
+
+        self.serial.flush()
